18a999ec6a7c75e30d9b8e82d45b5a448b786b23
Borislav Stanimirov <b.stanimirov@abv.bg>
2018-03-06 21:23:36 +0200

    Executable linux generation scripts

--- build/gen_makefile_release.sh
+++ build/gen_makefile_release.sh
@@ -2,5 +2,5 @@
 
 mkdir -p makefile_release
 cd makefile_release
-cmake ../.. -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles"
+cmake ../.. -DCMAKE_CXX_COMPILER=clang++-4.0 -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles"
 cd ..
